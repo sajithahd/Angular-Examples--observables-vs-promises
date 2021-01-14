852c547fd1eb39b08e3a1ab0328c75a6ffed0f8d
sajithahd <hdsajitha@gmail.com>
2021-01-14 12:19:53 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,36 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  template: `
+    <h1>Hello {{ name }}!</h1>
+  `,
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
+export class HelloComponent {
   @Input() name: string;
+
+  greetingPoster: Promise<any>;
+
+  constructor() {
+    // Promise body will run eagerly. just at the creation
+    this.greetingPoster = new Promise((resolve, reject) => {
+      console.log("Inside the promise");
+      resolve("Welcom, Sajitha");
+    });
+
+    // this will call before then
+    console.log("Before calling then on Promise");
+
+    // then will be triggered at last
+    this.greetingPoster.then(res => {
+      console.log(`Inside then, Greeting from Promise: ${res}`);
+    });
+  }
 }

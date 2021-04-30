2338ec19634181bd72a972d9a8b60e5d9336487c
sajithahd <hdsajitha@gmail.com>
2021-04-30 22:57:10 +0530

    1 file modified

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -5,57 +5,55 @@ import { map } from "rxjs/operators";
 @Component({
   selector: "hello",
   template: `
-    <h1>Hello {{ name }}!</h1>
-    Promises<br />
-    <span [innerHtml]="greetingPostContent"></span>
+    <h1>{{ name }}!</h1>
+    <h3>Promises</h3>
+    <span [innerHtml]="promiseContent"></span>
     <br />
-    Observable<br />
-    <span [innerHtml]="greetingLadyContent"></span>
-  `,
-  styles: [
-    `
-      h1 {
-        font-family: Lato;
-      }
-    `
-  ]
+    
+    ===============================
+    <h3>Observable</h3>
+    <span [innerHtml]="observableContent"></span>
+  `
 })
 export class HelloComponent {
   @Input() name: string;
 
-  greetingPoster: Promise<any>;
-  greetingPostContent: string = "";
-  greetingLady: Observable<any>;
-  greetingLadyContent: string = "";
+  promise: Promise<any>;
+  promiseContent: string = "";
+  observable: Observable<any>;
+  observableContent: string = "";
 
   constructor() {
-    this.promise();
-    this.observable();
+    this.promises();
+    this.observables();
   }
 
-  promise() {
-    // Promise body will run eagerly. just at the creation
-    this.greetingPoster = new Promise((resolve, reject) => {
-      this.greetingPostContent += " Inside the promise. <br/>";
-      resolve("Welcom, Sajitha");
+  promises() {
+    // Before Promis cretation
+    this.promiseContent += "1. Just Before promise creation. <br/>";
+
+    // Promise funtion passed through the constructor will be executed eagerly. Jjust at the time of creation
+    this.promise = new Promise((resolve, reject) => {
+      this.promiseContent += "2. Inside the promise. <br/>";
+      resolve("6. Welcom, Sajitha");
     });
 
     // This will call before then
-    this.greetingPostContent += " Before calling then on Promise. <br/>";
+    this.promiseContent += "3. Before calling then on Promise. <br/>";
 
     // Then will be triggered at last
-    this.greetingPoster.then(res => {
-      this.greetingPostContent += ` Inside then, Greeting from Promise: ${res} <br/>`;
+    this.promise.then(res => {
+      this.promiseContent += `5. Inside then, Greeting from Promise: ${res} <br/>`;
     });
 
     // Promises are asynchrones
-    this.greetingPostContent += " After promise block. <br/>";
+    this.promiseContent += "4. After the promise 'then' block. <br/>";
   }
 
-  observable() {
+  observables() {
     // Once subcrption triggered only observer call back function will be executed
-    this.greetingLady = new Observable(observer => {
-      this.greetingLadyContent += "Inside obervable <br/>";
+    this.observable = new Observable(observer => {
+      this.observableContent += "Inside obervable <br/>";
       observer.next("WelCome Sajitha Observer <br/>");
       observer.complete();
     }).pipe(map(v => (v += "Insde Pipe <br/>")));
@@ -64,22 +62,22 @@ export class HelloComponent {
     // observable.pipe(mapTo('this will be retrun instead of source obervable content'))
 
     // This line will be displayed first
-    this.greetingLadyContent += "Before calling subscribe <br/>";
+    this.observableContent += "Before calling subscribe <br/>";
 
     // Once subscription triggered it will retrun here
-    const subscription = this.greetingLady.subscribe({
+    const subscription = this.observable.subscribe({
       next: value => {
-        this.greetingLadyContent += value;
+        this.observableContent += value;
       },
       complete: () => {
-        this.greetingLadyContent += "Done with greeting leady <br/>";
+        this.observableContent += "Done with greeting leady <br/>";
       }
     });
 
     subscription.unsubscribe();
 
     // Observables are synchrones or asynchrones
-    this.greetingLadyContent += " After subscription block. <br/>";
+    this.observableContent += " After subscription block. <br/>";
 
     // this.greetingLady.subscribe(
     //   value => {

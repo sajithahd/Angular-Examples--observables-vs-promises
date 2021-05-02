9877d9afd6fb1e56e257050d7df28bf8e64930f0
sajithahd <hdsajitha@gmail.com>
2021-05-02 20:20:47 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
 
-import { EagerVsLazy } from './examples/eager_lazy.component';
-
+import { IndexComponent } from "./examples/index.component";
+import { EagerVsLazy } from "./examples/eager_lazy.component";
 
 const routes: Routes = [
-  { path: '', redirectTo: '/', pathMatch: 'full' },
-  { path: 'eager_lazy', component: EagerVsLazy }
+  { path: "", redirectTo: "/index", pathMatch: "full" },
+  { path: "index", component: IndexComponent },
+  { path: "eager_lazy", component: EagerVsLazy }
 ];
 
 @NgModule({
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}

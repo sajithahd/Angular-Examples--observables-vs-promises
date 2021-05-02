97a59685cdeb90bc668e3d988701bdb6d1efb27d
sajithahd <hdsajitha@gmail.com>
2021-05-02 22:55:06 +0530

    1 file modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { RouterModule, Routes } from "@angular/router";
 
 import { IndexComponent } from "./examples/index.component";
 import { EagerVsLazy } from "./examples/1_eager_lazy.component";
+import { SingleVsMultiple } from "./examples/2_single_multiple.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/index", pathMatch: "full" },
   { path: "index", component: IndexComponent },
   { path: "eager_lazy", component: EagerVsLazy },
-  { path: "single_multiple", component: EagerVsLazy }
+  { path: "single_multiple", component: SingleVsMultiple }
 ];
 
 @NgModule({

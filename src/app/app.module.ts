26deea698a1b23df72087df06f736d3af56ce6bb
sajithahd <hdsajitha@gmail.com>
2021-05-02 22:59:42 +0530

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { IndexComponent } from "./examples/index.component";
 import { EagerVsLazy } from "./examples/1_eager_lazy.component";
+import { SingleVsMultiple } from "./examples/2_single_multiple.component";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, AppRoutingModule],
-  declarations: [AppComponent, IndexComponent, EagerVsLazy],
+  declarations: [AppComponent, IndexComponent, EagerVsLazy, SingleVsMultiple],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
